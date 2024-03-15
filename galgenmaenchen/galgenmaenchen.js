ae3a10048db424d6218a65a009ef25f877a7074f
bch-af_fun0013 <bch-af_fun0013@flender-group.com>
2024-03-15 14:40:50 +0100

    clean code

---
+++ galgenmaenchen/galgenmaenchen.js
@@ -0,0 +1,41 @@
+
+// Add your JavaScript code here
+// Example:
+const words = ["apfel", "banane", "kirsche", "dattel", "holunderbeere"];
+let word = "";
+let guessedLetters = [];
+
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+function displayWord() {
+    const wordDisplay = document.getElementById("word-display");
+    wordDisplay.textContent = word
+        .split("")
+        .map(letter => (guessedLetters.includes(letter) ? letter : "_"))
+        .join(" ");
+}
+
+function displayGuesses() {
+    const guessesDisplay = document.getElementById("guesses");
+    guessesDisplay.textContent = `Erratene Buchstaben: ${guessedLetters.join(", ")}`;
+}
+
+function startGame() {
+    word = getRandomWord();
+    guessedLetters = [];
+    displayWord();
+    displayGuesses();
+}
+
+document.addEventListener("keydown", event => {
+    const letter = event.key.toLowerCase();
+    if (/[a-z]/.test(letter) && !guessedLetters.includes(letter)) {
+        guessedLetters.push(letter);
+        displayWord();
+        displayGuesses();
+    }
+});
+
+startGame();
