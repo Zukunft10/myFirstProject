369d2f959ae96ac04bd4e84292062fd66bb10648
bch-af_fun0013 <bch-af_fun0013@flender-group.com>
2024-03-18 15:12:29 +0100

    Typ wrong

--- galgenmaenchen/galgenmaenchen.js
+++ galgenmaenchen/galgenmaenchen.js
@@ -1,9 +1,10 @@
 
 // Add your JavaScript code here
 // Example:
-const words = ["apfel", "banane", "kirsche", "dattel", "holunderbeere", "fenster", "schrank", "haus", "baum", "richter", "kirche", "schleichen", "kreischen", "Wolken", "ananas"];
+const words = ["apfel", "banane", "kirsche", "dattel", "holunderbeere", "fenster", "schrank", "haus", "baum", "richter", "kirche", "schleichen", "kreischen", "wolken", "ananas"];
 let word = "";
 let guessedLetters = [];
+let typedWrong = 0;
 
 function getRandomWord() {
     return words[Math.floor(Math.random() * words.length)];
@@ -22,9 +23,21 @@ function displayGuesses() {
     guessesDisplay.textContent = `Erratene Buchstaben: ${guessedLetters.join(", ")}`;
 }
 
+
+function checkIfGuessedCorrectly(letter){
+    let result = word.includes(letter);
+    if (!result) {
+        typedWrong++;
+        const wrongDisplay = document.getElementById("typed-wrong-display");
+        wrongDisplay.textContent = typedWrong.toString();
+    }
+    return result;
+}
+
 function startGame() {
     word = getRandomWord();
     guessedLetters = [];
+    typedWrong = 0;
     displayWord();
     displayGuesses();
 }
@@ -33,6 +46,8 @@ document.addEventListener("keydown", event => {
     const letter = event.key.toLowerCase();
     if (/[a-z]/.test(letter) && !guessedLetters.includes(letter)) {
         guessedLetters.push(letter);
+
+        checkIfGuessedCorrectly(letter);
         displayWord();
         displayGuesses();
     }
